34151ce0ae720624cd969be31f5b7948fa87fd40
Jacob Phillips <jifalops@gmail.com>
2018-11-04 09:08:10 -0500

    wip, backing up crashed hd.

--- packages/mobile/lib/main.dart
+++ packages/mobile/lib/main.dart
@@ -1,109 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:jsonplaceholder/config.dart';
+import 'package:jsonplaceholder/themes.dart';
+import 'package:shared_theme_flutter/shared_theme_flutter.dart' as themer;
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _AppState extends State<App> {
+  themer.Theme theme;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    // Set a default theme synchronously. If you want to wait until the user's
+    // preferred theme is read from SharedPreferences, you'll need to show some
+    // UI to the user in the mean time, such as a spinner or a splash screen.
+    _setTheme(themeset.themes.first);
+    print('Theme: $theme');
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    // Lookup the preferred theme.
+    _readTheme();
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+  Widget build(BuildContext context) => MaterialApp(
+        title: appName,
+        theme: themer.themeData(theme),
+        home: Scaffold(
+          appBar: AppBar(
+              title: Text(appName,
+                  style: themer.textStyle(theme.fonts.title).copyWith(
+                      color: themer.contrastOf(theme.colors.primary))),
+              actions: <Widget>[_buildThemeSwitch()]),
+          body: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SingleChildScrollView(child: DemoItems()),
+          ),
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+
+  void _setTheme(themer.Theme t) {
+    print('Theme: $t');
+    theme = t;
+    themer.currentTheme = t;
+  }
+
+  Widget _buildThemeSwitch() =>
+      Row(crossAxisAlignment: CrossAxisAlignment.center, children: <Widget>[
+        Text(
+          'Dark',
+          style: themer.textStyleColored(
+              theme.fonts.title, themer.contrastOf(theme.colors.primary)),
+        ),
+        Switch(
+          value: theme.brightness == themer.Brightness.dark,
+          onChanged: (enabled) => _toggleTheme(),
+        )
+      ]);
+
+  void _readTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    try {
+      final name = prefs.getString('theme');
+      setState(() => _setTheme(themeset.getThemeByName(name)));
+    } catch (noSavedTheme) {}
+  }
+
+  void _toggleTheme() async {
+    setState(() => _setTheme(theme == themeset.themes.first
+        ? themeset.themes.last
+        : themeset.themes.first));
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('theme', theme.name);
+  }
+}
+
+class DemoItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = themer.currentTheme;
+    void _showSnackBar() => Scaffold.of(context)
+        .showSnackBar(SnackBar(content: Text("I'm a SnackBar.")));
+    Widget _colorWidget(themer.ContrastingColors colors, String text) =>
+        _buildColorWidget(context, colors, text);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+        Text('Display4', style: Theme.of(context).textTheme.display4),
+        Text('Display3', style: Theme.of(context).textTheme.display3),
+        Text('Display2', style: Theme.of(context).textTheme.display2),
+        Text('Display1', style: Theme.of(context).textTheme.display1),
+        Text('Headline', style: Theme.of(context).textTheme.headline),
+        Text('Title', style: Theme.of(context).textTheme.title),
+        Text('Subhead', style: Theme.of(context).textTheme.subhead),
+        Text('Body2', style: Theme.of(context).textTheme.body2),
+        Text('Body1', style: Theme.of(context).textTheme.body1),
+        Text('Button', style: Theme.of(context).textTheme.button),
+        Text('Caption', style: Theme.of(context).textTheme.caption),
+        _colorWidget(theme.colors.primary, 'Primary'),
+        _colorWidget(theme.colors.primaryLight, 'Primary Light'),
+        _colorWidget(theme.colors.primaryDark, 'Primary Dark'),
+        _colorWidget(theme.colors.secondary, 'Secondary ("Accent")'),
+        _colorWidget(theme.colors.secondaryLight, 'Secondary Light'),
+        _colorWidget(theme.colors.secondaryDark, 'Secondary Dark'),
+        _colorWidget(theme.colors.background, 'Background'),
+        _colorWidget(theme.colors.background.invert(), 'Background (inverted)'),
+        _colorWidget(theme.colors.card, 'Card'),
+        _colorWidget(theme.colors.divider, 'Divider'),
+        _colorWidget(theme.colors.error, 'Error'),
+        _colorWidget(theme.colors.notice, 'Notice'),
+        _colorWidget(theme.colors.indicator, 'Indicator'),
+        _colorWidget(theme.colors.hint, 'Hint'),
+        _colorWidget(theme.colors.selectedRow, 'SelectedRow'),
+        SizedBox(height: 12.0),
+        themer.primaryButton(_showSnackBar, text: 'Primary Button'),
+        SizedBox(height: 12.0),
+        themer.secondaryButton(_showSnackBar, text: 'Secondary Button'),
+        themer.tertiaryButton(_showSnackBar, text: 'Tertiary Button'),
+        themer.wrapInput(
+            TextField(decoration: InputDecoration(labelText: 'Input'))),
+      ],
     );
   }
+
+  Widget _buildColorWidget(
+          BuildContext context, themer.ContrastingColors colors, String text) =>
+      Container(
+          alignment: Alignment.center,
+          height: 56.0,
+          width: 256.0,
+          color: themer.colorOf(colors, Theme.of(context).backgroundColor),
+          child: Text(text,
+              style: Theme.of(context).textTheme.body2.copyWith(
+                  color: themer.contrastOf(
+                      colors, Theme.of(context).textTheme.body2.color))));
 }
